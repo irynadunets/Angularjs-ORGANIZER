cca4be0a803ff631ee902d4924bad53904d3a3cb
irynadunets <idunets@yahoo.com>
2017-07-28 00:05:14 -0700

    ANGULAR OGINIZER
    CLOCK+CALCULATOR+RECORDER

--- orginizer/public_html/app.js
+++ orginizer/public_html/app.js
@@ -35,5 +35,24 @@ function CalculatorCtrl()
         }
     }
 }
-
+myApp.controller('Ctrl', Ctrl);
+function Ctrl(){
+  this.edit=false;
+  this.books = [
+      "Otelli",
+      "Aleri",
+      "GOTO"   
+  ];
+  this.addnewBook=function(){
+      this.books.push(this.newbook);
+      this.newbook="";
+  }
+  this.Edit=function(){
+      this.edit=!this.edit;
+  }
+  
+  this.Delete=function(index){
+      this.books.splice(index,1);
+  }
+  }
 
